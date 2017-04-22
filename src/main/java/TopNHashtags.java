416075979f6bd31fade28866946b7f4217a28124
michael <vriesdemichael@gmail.com>
2017-04-22 17:07:57 +0200

    Expanded the topology to print tweets that have a trending tag attached.

--- src/main/java/TopNHashtags.java
+++ src/main/java/TopNHashtags.java
@@ -1,9 +1,16 @@
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction;
+import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -21,12 +28,13 @@ import org.apache.sling.commons.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Properties;
+
+import static java.lang.Math.toIntExact;
 
 public class TopNHashtags {
 
@@ -116,49 +124,69 @@ public class TopNHashtags {
 
         LOG.debug(String.format("Starting TopNHashtags with: N=%d, window size=%d, window slide=%d, redis=%s%d", n, windowSize, windowSlide, redisHost, redisPort));
 
-        env
+
+        SingleOutputStreamOperator<Tuple3<Long, String, String>> mappedStatuses = env
             /*
              * Get Data from the TwitterSource
              * Returns a JSON String representation of a status update
              */
             .addSource(twitter, "Twitter")
-
             /*
              * JSON String -> multiple (<hashtag>, <count>)
              * Maps the hashtags listed in the JSON String.
              */
-            .flatMap((FlatMapFunction<String, HashTagCount>) (value, out) -> {
-                try {
-                    JSONArray hashTags = new JSONObject(value).getJSONObject("entities").getJSONArray("hashtags");
-                    for (int i = 0; i < hashTags.length(); i++) {
-                        HashTagCount hashTag = new HashTagCount(hashTags.getJSONObject(i).getString("text").toLowerCase(),
-                                hashTags.getJSONObject(i).getJSONArray("indices").length() / 2);
-                        out.collect(hashTag);
-                    }
-                } catch (JSONException | ArrayIndexOutOfBoundsException e) {/* Skip */}
+            .flatMap(new FlatMapFunction<String, Tuple3<Long, String, String>>() {
+                @Override
+                public void flatMap(String value, Collector<Tuple3<Long, String, String>> collector) throws Exception {
+                    try {
+                        JSONObject status = new JSONObject(value);
+                        String statusText = status.getString("text");
+                        Long statusID = status.getLong("id");
+
+                        JSONArray hashTags = status.getJSONObject("entities").getJSONArray("hashtags");
+                        StringBuilder hashTagString = new StringBuilder();
+                        for (int i = 0; i < hashTags.length(); i++) {
+                            if (hashTagString.length() > 0) {
+                                hashTagString.append(" ");
+                            }
+                            hashTagString.append(hashTags.getJSONObject(i).getString("text").toLowerCase());
+                        }
+
+                        collector.collect(new Tuple3<Long, String, String>(statusID, statusText, hashTagString.toString()));
+
+                    } catch (JSONException | ArrayIndexOutOfBoundsException e) {/* Skip */}
+                }
             })
-
-            /* A helper function for previous flatmap lambda */
-            .returns(HashTagCount.class)
-
             /*
              * Assign timestamps and Watermarks, these are needed to make use of a TimeWindow
              * A better way is to do this is the Source, but TwitterSource does not do this.
              */
-            .assignTimestampsAndWatermarks(
-                new AssignerWithPeriodicWatermarks<HashTagCount>() {
-                    @Nullable
-                    @Override
-                    public Watermark getCurrentWatermark() {
-                        return new Watermark(System.currentTimeMillis() - 1000L);
-                    }
+            .assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<Tuple3<Long, String, String>>() {
+                @Override
+                public Watermark getCurrentWatermark() {
+                    return new Watermark(System.currentTimeMillis() - 1000L);
+                }
 
-                    @Override
-                    public long extractTimestamp(HashTagCount element, long previousElementTimestamp) {
-                        return System.currentTimeMillis();
-                    }
-                })
+                @Override
+                public long extractTimestamp(Tuple3<Long, String, String> element, long previousElementTimestamp) {
+                    return System.currentTimeMillis();
+                }
+            });
 
+
+        SingleOutputStreamOperator<Tuple1<String>> tagsOnly = mappedStatuses.project(2);
+
+        SingleOutputStreamOperator<Tuple3<Integer, String, Long>> topNCalc = tagsOnly.flatMap(new FlatMapFunction<Tuple1<String>, Tuple2<String, Long>>() {
+            @Override
+            public void flatMap(Tuple1<String> value, Collector<Tuple2<String, Long>> out) throws Exception {
+                for (String tag : value.f0.trim().split(" ")) {
+                    if (tag.equals(" ") || tag.equals("")) {
+                        continue;
+                    }
+                    out.collect(new Tuple2<>(tag, 1L));
+                }
+            }
+        })
             /*
              *
              * Create a sliding time window, the first value is the length of the time window, the second
@@ -169,7 +197,7 @@ public class TopNHashtags {
              *               |---------------Window 2----------------|
              *                       |---------------Window 3----------------|
              */
-            .timeWindowAll(Time.seconds(windowSize), Time.seconds(windowSlide))
+                .timeWindowAll(Time.seconds(windowSize), Time.seconds(windowSlide))
 
             /*
              * The apply function specified in TopNTweetsAllWindowFunction is called. It implements
@@ -178,32 +206,62 @@ public class TopNHashtags {
              * With a window size of 300 seconds and a window interval of 60 seconds there will be 5 active
              * windows.
              */
-            .apply(new TopNTweetsAllWindowFunction())
+                .apply(new TopNTweetsAllWindowFunction());
 
-            /*
-             * The datastream is saved to a Sink, in this case Redis.
-             * For quick tests in your IDE you can replace .addSink(...) with .print()
-             */
-            .addSink(
-                new RedisSink<>(
-                    new FlinkJedisPoolConfig.Builder().setHost(redisHost).setPort(redisPort).build(),
-                    new RedisMapper<Tuple3<Integer, String, Integer>>() {
-                        @Override
-                        public RedisCommandDescription getCommandDescription() {
-                            return new RedisCommandDescription(RedisCommand.SET);
-                        }
 
-                        @Override
-                        public String getKeyFromData(Tuple3<Integer, String, Integer> data) {
-                            return "Top10-" + data.f0;
-                        }
+        /*
+         * The datastream is saved to a Sink, in this case Redis.
+         * For quick tests in your IDE you can replace .addSink(...) with .print()
+         */
+        topNCalc.addSink(
+            new RedisSink<>(
+                new FlinkJedisPoolConfig.Builder().setHost(redisHost).setPort(redisPort).build(),
+                new RedisMapper<Tuple3<Integer, String, Long>>() {
+                    @Override
+                    public RedisCommandDescription getCommandDescription() {
+                        return new RedisCommandDescription(RedisCommand.SET);
+                    }
 
-                        @Override
-                        public String getValueFromData(Tuple3<Integer, String, Integer> data) {
-                            return data.f1 + ", " + Integer.toString(data.f2);
+                    @Override
+                    public String getKeyFromData(Tuple3<Integer, String, Long> data) {
+                        return "Top10-" + data.f0;
+                    }
+
+                    @Override
+                    public String getValueFromData(Tuple3<Integer, String, Long> data) {
+                        return data.f1 + ", " + Long.toString(data.f2);
+                    }
+                }
+            )
+        ).name("Redis_" + redisHost + ":" + redisPort);
+
+        SingleOutputStreamOperator<Tuple2<Long, String>> topNTexts = mappedStatuses.connect(topNCalc).flatMap(
+            new CoFlatMapFunction<Tuple3<Long, String, String>, Tuple3<Integer, String, Long>, Tuple2<Long, String>>() {
+                String[] topN;
+
+                @Override
+                public void flatMap1(Tuple3<Long, String, String> value, Collector<Tuple2<Long, String>> out) throws Exception {
+                    for (String tag : value.f2.split(" ")) {
+                        if (ArrayUtils.contains(topN, tag)) {
+                            out.collect(new Tuple2<>(value.f0, value.f1));
+                            break;
                         }
                     }
-                )).name("Redis_" + redisHost + ":" + redisPort);
+                }
+
+                @Override
+                public void flatMap2(Tuple3<Integer, String, Long> value, Collector<Tuple2<Long, String>> out) throws Exception {
+                    if (topN == null) {
+                        topN = new String[n];
+                    }
+                    topN[value.f0 - 1] = value.f1;
+                }
+            }
+        );
+
+
+
+        topNTexts.map(value -> "Status: \t" + value.f1).returns(String.class).print();
 
         /*
          *  Actually execute the streaming plan defined above.
@@ -213,33 +271,34 @@ public class TopNHashtags {
 
     }
 
+
     /**
      * Apply function for aggregating Tweet counts within a window.
      * Listed seperately because of its size.
      */
-    static class TopNTweetsAllWindowFunction implements AllWindowFunction<HashTagCount, Tuple3<Integer, String, Integer>, TimeWindow> {
+    static class TopNTweetsAllWindowFunction implements AllWindowFunction<Tuple2<String, Long>, Tuple3<Integer, String, Long>, TimeWindow> {
 
         @Override
-        public void apply(TimeWindow window, Iterable<HashTagCount> values, Collector<Tuple3<Integer, String, Integer>> out) throws Exception {
+        public void apply(TimeWindow window, Iterable<Tuple2<String, Long>> values, Collector<Tuple3<Integer, String, Long>> out) throws Exception {
             LOG.debug("Window finished");
 
             // List of all entries in this window.
-            List<HashTagCount> entries = new LinkedList<>();
+            List<Tuple2<String, Long>> entries = new LinkedList<>();
 
 
-            for (HashTagCount tag : values) {
+            for (Tuple2<String, Long> tag : values) {
                 if (entries.size() == 0 ) {
                     entries.add(tag);
                 }
 
-                ListIterator<HashTagCount> iter = entries.listIterator();
+                ListIterator<Tuple2<String, Long>> iter = entries.listIterator();
 
                 boolean modified = false;
                 while (iter.hasNext()) {
-                    HashTagCount h = iter.next();
+                    Tuple2<String, Long> h = iter.next();
 
-                    if (h.getTag().equals(tag.getTag())) {
-                        iter.set(new HashTagCount(tag.getTag(), h.getCount() + tag.getCount()));
+                    if (h.f0.equals(tag.f0)) {
+                        iter.set(new Tuple2<>(tag.f0, h.f1 + tag.f1));
                         modified = true;
                         break;
                     }
@@ -249,12 +308,12 @@ public class TopNHashtags {
                 }
             }
 
-            entries.sort((o1, o2) -> o2.getCount() - o1.getCount());
+            entries.sort((o1, o2) -> toIntExact(o2.f1 - o1.f1));
 
             int collectSize = entries.size() >= n ? n : entries.size();
             for (int i = 0; i < collectSize; i++) {
                 LOG.info((i + 1) + ") " + entries.get(i));
-                out.collect(new Tuple3<>((i+1), entries.get(i).getTag(), entries.get(i).getCount()));
+                out.collect(new Tuple3<>((i+1), entries.get(i).f0, entries.get(i).f1));
             }
 
             entries.clear();
